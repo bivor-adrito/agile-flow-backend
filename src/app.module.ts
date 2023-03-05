0fe622f1d67e1bb66707eef378afece023678992
amanullha <aullah060@gmail.com>
2023-03-05 14:23:47 +0600

    mongodb setup done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './modules/user/user.controller';
 import { UserService } from './modules/user/user.service';
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@agileflow.dhlktmi.mongodb.net/?retryWrites=true&w=majority`)
+  ],
   controllers: [
     AppController,
     UserController
   ],
-  providers: [AppService,UserService],
+  providers: [AppService, UserService],
 })
-export class AppModule {}
+export class AppModule { }
